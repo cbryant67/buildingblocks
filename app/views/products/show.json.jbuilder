3565f4e5726dd566963fd2e8bd3d05548f00f2d1
Chris Bryant <chris@whiteclay.com>
2016-08-04 15:45:52 -0400

    Clean

--- app/views/products/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @product, :id, :title, :description, :price, :category, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'cart/index'
-
-  resources :products
   devise_for :users
    devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
